e448cc133a9be29d208ec4f0c9a0877bece8b75c
Agustin Crescio <crescioagustin@gmail.com>
2022-06-05 18:47:01 -0300

    cambios del nav

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import img from "./img/lentes1.jpg"
+import NavBar from "./components/NavBar";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+
+        <h1 className="text-center ">
+          Bienvenido a mi App
+        </h1>
+      </header>
+      </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+//Comenzar en Mayuscula: Pascal Case
+//Exportarlo
+//Importarlo en App.js
+
+
+export default function     NavBar () {
+    return (
+        <nav>
+            <ul className="nav justify-content-center">
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Productos</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Contacto</a>
+            </li>
+        </ul>
+            </nav>
+      
+    );
+  }
+  
+;
+  
